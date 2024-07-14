62fd0629863dd1657babe68d91b93105e712fc3b
s.asadiyan <s.asadiyan@bluteam.ir>
2024-07-14 18:04:09 +0330

    move on

---
+++ src/main/java/me/sepehrasadiyan/wallet_v2/configuration/MicrometerConfiguration.java
@@ -0,0 +1,23 @@
+package me.sepehrasadiyan.wallet_v2.configuration;
+
+
+import io.micrometer.core.aop.CountedAspect;
+import io.micrometer.core.aop.TimedAspect;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MicrometerConfiguration {
+
+    @Bean
+    public TimedAspect timedAspect(MeterRegistry registry) {
+        return new TimedAspect(registry);
+    }
+
+    @Bean
+    public CountedAspect countedAspect(MeterRegistry registry){
+        return new CountedAspect(registry);
+    }
+
+}
