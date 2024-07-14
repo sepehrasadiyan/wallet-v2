6bec1aa21ef32877fc94e6ba39be22e404a1731b
s.asadiyan <s.asadiyan@bluteam.ir>
2024-07-14 19:00:03 +0330

    move on

--- src/main/java/me/sepehrasadiyan/wallet_v2/common/response/DepositResponseDto.java
+++ src/main/java/me/sepehrasadiyan/wallet_v2/common/response/DepositResponseDto.java
@@ -1,4 +1,13 @@
 package me.sepehrasadiyan.wallet_v2.common.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.ToString;
+
+@ToString
+@Data
+@AllArgsConstructor
 public class DepositResponseDto {
+    private String resourceId;
+
 }
