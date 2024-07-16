6b472fb603aa45a82c467f61996cd9bf2d26552f
sepehr <asadiyansepehr@gmail.com>
2024-07-16 16:17:12 +0330

    finish job

--- src/main/java/me/sepehrasadiyan/wallet_v2/WalletV2Application.java
+++ src/main/java/me/sepehrasadiyan/wallet_v2/WalletV2Application.java
@@ -2,10 +2,12 @@ package me.sepehrasadiyan.wallet_v2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
+@EnableScheduling
 public class WalletV2Application {
 
 	public static void main(String[] args) {
--- src/main/java/me/sepehrasadiyan/wallet_v2/repository/SimpleJournalRepository.java
+++ src/main/java/me/sepehrasadiyan/wallet_v2/repository/SimpleJournalRepository.java
@@ -2,8 +2,21 @@ package me.sepehrasadiyan.wallet_v2.repository;
 
 import me.sepehrasadiyan.wallet_v2.domain.SimpleJournal;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
 public interface SimpleJournalRepository extends JpaRepository<SimpleJournal, Long> {
+    @Query("SELECT s.id FROM SimpleJournal s")
+    @Transactional(propagation = Propagation.NOT_SUPPORTED, readOnly = true)
+    List<Long> findAllIds();
+
+    @Query("SELECT s FROM SimpleJournal s WHERE s.id IN :ids")
+    @Transactional(propagation = Propagation.NOT_SUPPORTED, readOnly = true)
+    List<SimpleJournal> findByIds(@Param("ids") List<Long> ids);
 }
